4f2a7d8feffa30be29561bc97a6ac8a9bfe11ad5
honki12345 <iui4793@gmail.com>
2023-12-18 16:33:33 +0900

    feat: Prob007

---
+++ src/test/java/chapter01/Prob007.java
@@ -0,0 +1,15 @@
+package chapter01;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+// 문자열을 int, long, float, double로 변환
+public class Prob007 {
+
+    /*String을 int, long, float, double로 변환하는 방법은
+    Integer.parseInt(), Long.parseLong(), Float.parseFloat(), Double.parseDouble()을 이용하는 것*/
+
+    /*String을 Integer나 Long, Float, Double 객체로 변환하는 방법은
+    * Integer.valueOf(), Long.valueOf(), Float.valueOf(), Double.valueOf() 를 이용하는것*/
+
+}
